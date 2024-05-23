c1104d0e9c7e08f373508fcc9f7645ce042e15cd
Olumide Adenigba <Olumideadenigba@gmail.com>
2024-05-23 22:08:57 +0100

    Validation added

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController.js');
 const auth = require('../middleware/auth.js');
+const validator = require('../validators/authValidator.js')
 
 //For user registration and login
-router.post('/register', auth.register);
-router.post('/login', auth.login);
+router.post('/register', validator.signupValidation, auth.register);
+router.post('/login', validator.loginValidation, auth.login);
 
 
 module.exports = router;
---
+++ src/validators/authValidator.js
@@ -0,0 +1,48 @@
+const joi = require('joi')
+
+const signupValidation = async (req, res, next) => {
+    try {
+        const bodyofRequest = req.body;
+        const signupSchema = joi.object({
+            email: joi.string().email().required(),
+            firstName: joi.string().required(),
+            lastName: joi.string().required(),
+            password: joi.string().pattern(new RegExp('^[a-zA-Z0-9@#]{3,30}$')).required(),
+            token: joi.array().items(joi.string().valid('x', 'y', 'z'))
+        })
+
+        await signupSchema.validateAsync(bodyofRequest, { abortEarly: true })
+
+        next()
+    } catch (error) {
+        return res.status(422).json({
+            message: error.message,
+            success: false
+        })
+    }
+}
+
+
+const loginValidation = async (req, res, next) => {
+    try {
+        const loginSchema = joi.object({
+            password: joi.string().required(),
+            email: joi.string().email().required(),
+        })
+
+        await loginSchema.validateAsync(req.body, { abortEarly: true })
+    
+        next()
+    } catch (error) {
+        return res.status(422).json({
+            message: error.message,
+            success: false
+        })
+    }
+}
+
+
+module.exports = {
+    signupValidation,
+    loginValidation
+}
